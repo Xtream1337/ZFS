497a2041e937101aa2729f1f1d6bed3571b5c0e0
Mihail Kolev <36764541+BABAMIZAIKAL@users.noreply.github.com>
2019-03-15 17:35:25 +0200

    PY Connection1

---
+++ python test/client.py
@@ -0,0 +1,23 @@
+import time
+import socket
+import sys
+
+print("Welcome to chat")
+time.sleep(1)
+
+s = socket.socket()
+print("")
+host = input(str("Server adress : "))
+name = input(str("Username:"))
+port = 8080
+print("trying to connect  to ", host, " at port", port)
+time.sleep(1)
+s.connect((host, port))
+print("connected")
+
+##connection done###
+
+s.send(name.encode())
+s_name = s.recV(1024)
+s_name = s_name.decode()
+print(s_name, "Has joined the chat")
---
+++ python test/host.py
@@ -0,0 +1,28 @@
+import time
+import socket
+import sys
+
+print("Welcome to chat")
+time.sleep(1)
+
+s = socket.socket()
+host = socket.gethostname()
+port = 8080
+s.bind((host,port))
+print(host)
+name = input(str("Username : "))
+
+s.listen(1)
+
+print("Waiting for connection")
+
+conn, addr = s.accept()
+print ("recieved connection")
+
+
+#connection done###
+
+s_name = conn.recv(1024)
+s_name = s_name.decode()
+print(s_name, "Has connected to chat room")
+conn.send(name.encode())
